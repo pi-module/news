5745c563dbde3559d38798d7a7e36a0951df5026
voltan <djvoltan@gmail.com>
2014-04-21 20:43:25 +0430

    Update language files

--- config/page.php
+++ config/page.php
@@ -14,26 +14,32 @@ return array(
     // Admin section
     'admin' => array(
         array(
+            'title'         => _a('Attach'),
             'controller'    => 'attach',
             'permission'    => 'attach',
         ),
         array(
+            'title'         => _a('Extra'),
             'controller'    => 'extra',
             'permission'    => 'extra',
         ),
         array(
+            'title'         => _a('Spotlight'),
             'controller'    => 'spotlight',
             'permission'    => 'spotlight',
         ),
         array(
+            'title'         => _a('Story'),
             'controller'    => 'story',
             'permission'    => 'story',
         ),
         array(
+            'title'         => _a('Tools'),
             'controller'    => 'tools',
             'permission'    => 'tools',
         ),
         array(
+            'title'         => _a('Topic'),
             'controller'    => 'topic',
             'permission'    => 'topic',
         ),
@@ -41,42 +47,50 @@ return array(
     // Front section
     'front' => array(
         array(
+            'title'         => _a('Index page'),
             'controller'    => 'index',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Topic'),
             'controller'    => 'topic',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Story'),
             'controller'    => 'story',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Tag'),
             'controller'    => 'tag',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Writer'),
             'controller'    => 'writer',
             'permission'    => 'writer',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Archive'),
             'controller'    => 'archive',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Topic list'),
             'controller'    => 'topic',
             'action'        => 'list',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Tag list'),
             'controller'    => 'tag',
             'action'        => 'list',
             'permission'    => 'public',
