66c4fee578fce99e8556720da6aad3ab37d1b436
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2020-11-15 16:16:28 +0330

    Fix bug in query

--- src/Controller/Front/IndexController.php
+++ src/Controller/Front/IndexController.php
@@ -201,7 +201,6 @@ class IndexController extends ActionController
         $page   = $this->params('page', 1);
         $order  = $this->setLinkOrder($order);
         $module = $this->params('module');
-        $where  = [];
 
         // Get config
         $config = Pi::service('registry')->config->read($module);
